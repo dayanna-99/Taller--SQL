19bbc2b7bc940b0c9991e5287df25b56428af4de
Dayanna-99 <dayana.oc1999@gmail.com>
2026-02-25 20:26:43 -0500

    Solucion 12 ejercicio

---
+++ 12 Ejercicio.sql
@@ -0,0 +1,8 @@
+-- Ejercicio 12
+-- Precio promedio de los productos
+
+SELECT AVG(precio) AS precio_promedio
+FROM productos;
+
+-- AVG(precio) calcula la media aritmética
+--de todos los precios registrados.
